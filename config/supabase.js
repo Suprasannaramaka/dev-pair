a29a554dde4e10a488cde18a479387c6eb070f6f
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-01-15 00:57:58 +0530

    init

--- config/supabase.js
+++ config/supabase.js
@@ -4,6 +4,10 @@
     const supabaseKey = process.env.SUPABASE_KEY;
     const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
+    if (!supabaseUrl || !supabaseKey || !supabaseServiceKey) {
+  throw new Error("❌ Supabase environment variables are missing");
+}
+
     // Client for public operations
     export const supabase = createClient(supabaseUrl, supabaseKey);
 
--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const app = express();
 /* ------------------ Middlewares ------------------ */
 app.use(
   cors({
+
     origin: process.env.FRONTEND_URL,
     credentials: true,
   })
